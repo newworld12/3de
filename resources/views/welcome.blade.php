bf39f0a1125635e33190ba39629723f46d823ef1
michaelmj2323 <michaelmj2323@gmail.com>
2017-10-01 16:46:16 +0200

    safssfsfsf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel hellow gal
+                    Laravel hexcxcllow gal
                 </div>
 
                 <div class="links">
